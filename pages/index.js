c7f5a0172d5441fc842f7b22a4f407558b0c20af
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2023-03-14 20:09:40 +0100

    feat: install ethersjs and connect to wallet

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,79 @@
 import Head from 'next/head'
+import { ethers } from 'ethers'
+import { useEffect, useState } from 'react'
+
 export default function Home () {
+  const [currentAccount, setCurrentAccount] = useState('')
+  const [balance, setBalance] = useState('')
+  const [chainId, setChainId] = useState('')
+  const [errorLogin, setErrorLogin] = useState('')
+
+  const truncateAddress = (address) => {
+    return address.slice(0, 6) + '...' + address.slice(-4)
+  }
+
+  useEffect(() => {
+    window.ethereum.on('accountsChanged', function (accounts) {
+      console.log('Change account on metamask', accounts[0])
+      setCurrentAccount(accounts[0])
+    })
+
+    window.ethereum.on('networkChanged', function (networkId) {
+      console.log('Change chain on metamask', networkId)
+      setChainId(networkId)
+    })
+  }, [])
+
+  useEffect(() => {
+    setErrorLogin('')
+
+    if (!currentAccount || !ethers.utils.isAddress(currentAccount)) {
+      return
+    }
+    if (!window.ethereum) {
+      setErrorLogin('Please install MetaMask')
+      return
+    }
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    provider.getNetwork().then((result) => {
+      console.log('chain ', result.chainId)
+      if (result.chainId !== 5) {
+        setErrorLogin('You have to switch on Goerli network')
+        return
+      }
+      setChainId(result.chainId)
+    })
+
+    provider.getBalance(currentAccount).then((result) => {
+      console.log('balance ', ethers.utils.formatEther(result))
+      setBalance(ethers.utils.formatEther(result))
+    })
+  }, [currentAccount, chainId])
+
+  const onClickConnect = () => {
+    // client side code
+    if (!window.ethereum) {
+      setErrorLogin('Please install MetaMask')
+      return
+    }
+
+    // we can do it using ethers.js
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+    // MetaMask requires requesting permission to connect users accounts
+    provider.send('eth_requestAccounts', [])
+      .then((accounts) => {
+        if (accounts.length > 0) setCurrentAccount(accounts[0])
+      })
+      .catch((e) => console.log(e))
+  }
+
   return (
     <>
       <Head>
-        <title>Welcome to my app</title>
-        <meta name='description' content='Welcome to my app' />
+        <title>Login with your NFT</title>
+        <meta name='description' content='Login with your NFT' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
@@ -23,17 +92,26 @@ export default function Home () {
                       <div className='md:mx-6 md:p-12'>
                         <div className='text-center'>
                           <h4 className='mt-1 mb-12 pb-1 text-xl font-semibold'>
-                            Auth with NFT
+                            Auth with NFT - {balance} - {chainId}
                           </h4>
                         </div>
                         <form>
                           <div className='mb-12 pt-1 pb-1 text-center'>
-                            <button
-                              className='mb-3 inline-block w-full rounded px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] backgroundGradiant'
-                              type='button'
-                            >
-                              Log in with Metamask
-                            </button>
+                            {currentAccount
+                              ? <p>{truncateAddress(currentAccount)}</p>
+                              : (
+                                <button
+                                  className='mb-3 inline-block w-full rounded px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] backgroundGradiant'
+                                  type='button'
+                                  onClick={onClickConnect}
+                                >
+                                  Log in with Metamask
+                                </button>
+                                )}
+
+                            {errorLogin !== '' && (
+                              <p>{errorLogin}</p>
+                            )}
                           </div>
                           <div className='flex items-center justify-between pb-6'>
                             <p className='mb-0 mr-2'>Don't have an NFT?</p>
