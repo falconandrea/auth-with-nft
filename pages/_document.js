09f19a1454edd9fbaa35ca4ad571b7a5ac1430f9
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2023-03-14 19:13:34 +0100

    feat: config nextjs and add tailwind and start homepage

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
+export default function Document () {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
+    <Html lang='en'>
+      <Head>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+        <link href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap' rel='stylesheet' />
+      </Head>
+      <body className='bg-neutral-200 dark:bg-neutral-700'>
         <Main />
         <NextScript />
       </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {},
+  plugins: []
 }
