bfaf131cb57d6a5231eef3091bac3c6ba52e9f66
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2023-03-15 18:40:20 +0100

    feat: init whitelist contract

---
+++ contracts/Whitelist.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract Whitelist {
+  address owner;
+  mapping(address => bool) addresses;
+
+  constructor() {
+    owner = msg.sender;
+  }
+
+  function addUser(address _address) public onlyOwner {
+    require(!isInWhitelist(_address), 'Address already in whitelist');
+    addresses[_address] = true;
+  }
+
+  function removeUser(address _address) public onlyOwner {
+    require(isInWhitelist(_address), 'Address not in whitelist');
+    delete addresses[_address];
+  }
+
+  function isInWhitelist(address _address) public view returns(bool) {
+    return addresses[_address];
+  }
+
+  modifier onlyOwner {
+    require(msg.sender == owner, 'You are not the owner');
+    _;
+  }
+  modifier isWhitelisted {
+    require(isInWhitelist(msg.sender), 'Address not in whitelist');
+    _;
+  }
+
+}
